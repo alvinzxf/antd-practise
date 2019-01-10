1560ddbcee2b7207e4f582de5f196df976d25f39
alvin <alvinzxf@163.com>
2019-01-10 12:11:30 +0800

    add models

--- config/config.js
+++ config/config.js
@@ -6,9 +6,7 @@ export default {
       {
         // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
         antd: true,
-        dva: {
-          hmr: true,
-        },
+        dva: true,
       }
     ],
   ],
@@ -26,6 +24,7 @@ export default {
         path: '/helloworld',
         component: 'Helloworld'
       },
+      { path: '/puzzlecards', component: 'PuzzlecardSPage' },
       {
         path: './dashboard',
         routes: [
