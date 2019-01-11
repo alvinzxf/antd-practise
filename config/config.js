35a776855edd2b9e299451e01355ab5df0cf9b53
alvin <alvinzxf@163.com>
2019-01-11 09:53:28 +0800

    add mock

--- config/config.js
+++ config/config.js
@@ -7,6 +7,12 @@ export default {
         // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
         antd: true,
         dva: true,
+        //Locale enable 为true将运行本地mock模拟数据
+        locale: {
+          enable: true, // default false
+          default: 'zh-CN', // default zh-CN
+          baseNavigator: true, // default true, when it is true, will use `navigator.language` overwrite default
+        },
       }
     ],
   ],
@@ -44,7 +50,7 @@ export default {
    */
   // proxy: {
   //   '/dev': {
-  //     // target: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com',
+  //     target: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com',
   //     changeOrigin: true,
   //   },
   // },
@@ -52,11 +58,11 @@ export default {
    * 如果是以 /test 开头的，那么就转发到远端
    * /test 也会保留在转发地址中
    */
-  proxy: {
-    '/test': {
-      target: 'http://114.116.89.193:8999',
-      changeOrigin: true,
-    },
-  },
+  // proxy: {
+  //   '/test': {
+  //     target: 'http://114.116.89.193:8999',
+  //     changeOrigin: true,
+  //   },
+  // },
 
 }
--- src/models/puzzlecards.js
+++ src/models/puzzlecards.js
@@ -17,9 +17,9 @@ export default {
   effects: {
     *queryInitCards(_, sagaEffects) {
       const { call, put } = sagaEffects;
-      // const endPointURI = '/dev/random_joke';
+      const endPointURI = '/dev/random_joke';
       // const endPointURI = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke';
-      const endPointURI = '/test/cardList';
+      // const endPointURI = '/test/cardList';
       const puzzle = yield call(request, endPointURI);
       yield put({ type: 'addNewCard', payload: puzzle });
 
